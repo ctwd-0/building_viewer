ea73bf526ce60ccca06b791171f32a2d443fcdc2
ctwd-0 <suncong10@gmail.com>
2018-04-05 23:19:13 +0800

    增加可收起展开的边栏

--- src/Viewer2.vue
+++ src/Viewer2.vue
@@ -1,14 +1,20 @@
 <template>
 	<div class = "outer_div" @drop="drop($event)" @dragover="allow_drop($event)">
 		<div class="main_frame" :style="{width:inner_width + 'px', height: inner_height + 'px'}">
-			<div class="webgl_reigon" :style="{width:webgl_reigon_width + 'px', height: webgl_reigon_height + 'px'}">
+			<div class="webgl" :style="{width:webgl_width + 'px', height: webgl_height + 'px'}">
 			</div>
-			<div class="right_region" :style="{width:right_region_width + 'px', height: inner_height + 'px'}">
-				<div class="right_region_resize" :style="{height: inner_height + 'px'}" draggable="true" @dragstart="start_drag($event, 'right')">
+			<div class="right" :style="{width:real_right_width + 'px', height: inner_height + 'px'}">
+				<div class="right_resize" :style="{height: inner_height + 'px'}" draggable="true" @dragstart="start_drag($event, 'right')">
+				</div>
+				<div class="right_tab_area" :style="{height: inner_height + 'px'}">
+					<label :class="{'right_tab':right_index !== index, 'right_tab_selected': right_index === index}" v-for="(tag, index) in right_tags" @click="right_click(index)">{{tag}}</label>
 				</div>
 			</div>
-			<div class="bottom_region" :style="{width:webgl_reigon_width + 'px', height: bottom_region_height + 'px'}">
-				<div class="bottom_region_resize" :style="{width:webgl_reigon_width + 'px'}" draggable="true" @dragstart="start_drag($event, 'bottom')">
+			<div class="bottom" :style="{width:webgl_width + 'px', height: real_bottom_height + 'px'}">
+				<div class="bottom_resize" :style="{width:webgl_width + 'px'}" draggable="true" @dragstart="start_drag($event, 'bottom')">
+				</div>
+				<div class="bottom_tab_area" :style="{width:webgl_width + 'px'}">
+					<label :class="{'bottom_tab':bottom_index !== index, 'bottom_tab_selected': bottom_index === index}" v-for="(tag, index) in bottom_tags" @click="bottom_click(index)">{{tag}}</label>
 				</div>
 			</div>
 		</div>
@@ -36,25 +42,62 @@ export default {
 		return {
 			inner_width: 1280,
 			inner_height: 720,
-			right_region_width: 300,
-			bottom_region_height: 240,
-			right_reigon_show: true,
-			bottom_region_show: true,
-			webgl_reigon_width: 980,
-			webgl_reigon_height: 480,
-			button_size: 20,
+			right_width: 300,
+			real_right_width: 300,
+			bottom_height: 240,
+			real_bottom_height: 240,
+			right_show: true,
+			bottom_show: true,
+			webgl_width: 980,
+			webgl_height: 480,
+			button_size: 26,
 			last_x: 0,
 			last_y: 0,
 			dragging: false,
+			right_index: 0,
+			bottom_index: 0,
+			right_tags: ["资料", "工具"],
+			bottom_tags: ["数据表", "表头选择"],
 		};
 	},
 
 	methods: {
+		right_click(index) {
+			if(this.right_show) {
+				if(index === this.right_index) {
+					this.right_show = false
+					this.right_index = -1
+				} else {
+					this.right_index = index
+				}
+			} else {
+				this.right_show = true
+				this.right_index = index
+			}
+			this.set_x_according()
+		},
+		
+		bottom_click(index) {
+			if(this.bottom_show) {
+				if(index === this.bottom_index) {
+					this.bottom_show = false
+					this.bottom_index = -1
+				} else {
+					this.bottom_index = index
+				}
+			} else {
+				this.bottom_show = true
+				this.bottom_index = index
+			}
+			this.set_y_according()
+		},
+
 		start_drag(event, type) {
 			event.dataTransfer.setData("type", type)
 			event.dataTransfer.setData("start_X", event.clientX)
 			event.dataTransfer.setData("start_Y", event.clientY)
 		},
+
 		drop(event) {
 			event.preventDefault()
 			let type = event.dataTransfer.getData("type")
@@ -67,58 +110,71 @@ export default {
 				this.resize_bottom(delta)
 			}
 		},
+
 		resize_right(delta) {
-			let dest_right_region_width = this.right_region_width - delta
-			if (dest_right_region_width > this.inner_width * .5) {
-				dest_right_region_width = this.inner_width * .5
-			} else if (dest_right_region_width < this.inner_width * .1) {
-				dest_right_region_width = this.inner_width * .1
+			let dest_right_width = this.right_width - delta
+			if (dest_right_width > this.inner_width * .5) {
+				dest_right_width = this.inner_width * .5
+			} else if (dest_right_width < this.inner_width * .1) {
+				dest_right_width = this.inner_width * .1
 			}
-			this.right_region_width = dest_right_region_width
+			this.right_width = dest_right_width
 			this.set_x_according()
 		},
+
 		resize_bottom(delta) {
-			let dest_bottom_region_height = this.bottom_region_height - delta
-			if(dest_bottom_region_height > this.inner_height * .5) {
-				dest_bottom_region_height = this.inner_height * .5
-			} else if (dest_bottom_region_height < this.inner_height * .1) {
-				dest_bottom_region_height = this.inner_height * .1
+			let dest_bottom_height = this.bottom_height - delta
+			if(dest_bottom_height > this.inner_height * .5) {
+				dest_bottom_height = this.inner_height * .5
+			} else if (dest_bottom_height < this.inner_height * .1) {
+				dest_bottom_height = this.inner_height * .1
 			}
-			this.bottom_region_height = dest_bottom_region_height
+			this.bottom_height = dest_bottom_height
 			this.set_y_according()
 		},
+
 		set_y_according() {
-			if (this.bottom_region_show) {
-				this.webgl_reigon_height = this.inner_height - this.bottom_region_height
+			if (this.bottom_show) {
+				this.webgl_height = this.inner_height - this.bottom_height
+				this.real_bottom_height = this.bottom_height
 			} else {
-				this.webgl_reigon_height = this.inner_height = this.button_size
+				this.webgl_height = this.inner_height - this.button_size
+				this.real_bottom_height = this.button_size
 			}
 		},
+
 		set_x_according() {
-			if (this.right_reigon_show) {
-				this.webgl_reigon_width = this.inner_width - this.right_region_width
+			if (this.right_show) {
+				this.webgl_width = this.inner_width - this.right_width
+				this.real_right_width = this.right_width
 			} else {
-				this.webgl_reigon_width = this.inner_width - this.button_size
+				this.webgl_width = this.inner_width - this.button_size
+				this.real_right_width = this.button_size
 			}
 		},
+
 		allow_drop(event) {
 			event.preventDefault()
 		},
+
 		calculate_paras_init() {
 			this.inner_width = window.innerWidth - 18 < 640 ? 640 : window.innerWidth - 18
 			this.inner_height = window.innerHeight - 18 < 480? 480: window.innerHeight - 18
-			this.right_region_width = this.inner_width * .3
-			this.bottom_region_height = this.inner_height * .3
-			this.webgl_reigon_width = this.inner_width * .7
-			this.webgl_reigon_height = this.inner_height * .7
+			this.right_width = this.inner_width * .3
+			this.bottom_height = this.inner_height * .3
+			this.webgl_width = this.inner_width * .7
+			this.webgl_height = this.inner_height * .7
+			this.set_x_according()
+			this.set_y_according()
 		},
+
 		calculate_paras() {
-			let width_ratio = this.right_region_width / this.inner_width
-			let height_ratio = this.bottom_region_height / this.inner_height
+			let width_ratio = this.right_width / this.inner_width
+			let height_ratio = this.bottom_height / this.inner_height
 			this.inner_width = window.innerWidth - 18 < 640 ? 640 : window.innerWidth - 18
 			this.inner_height = window.innerHeight - 18 < 480? 480: window.innerHeight - 18
-			this.right_region_width = this.inner_width * width_ratio
-			this.bottom_region_height = this.inner_height * height_ratio
+			this.right_width = this.inner_width * width_ratio
+			this.bottom_height = this.inner_height * height_ratio
 			this.set_x_according()
 			this.set_y_according()
 		}
@@ -145,7 +201,7 @@ export default {
 
 <style scoped>
 
-.webgl_reigon {
+.webgl {
 	position: absolute;
 	background-color: grey;
 	left: 0;
@@ -153,7 +209,7 @@ export default {
 	margin-left: 3px; 
 	margin-top: 3px; 
 }
-.right_region_resize {
+.right_resize {
 	position: absolute;
 	width: 3px;
 	left: 0;
@@ -162,7 +218,7 @@ export default {
 	cursor: e-resize;
 }
 
-.bottom_region_resize {
+.bottom_resize {
 	position: absolute;
 	height: 3px;
 	top: 0;
@@ -171,7 +227,7 @@ export default {
 	cursor: s-resize;
 }
 
-.right_region {
+.right {
 	position: absolute;
 	background-color: orange;
 	right: 0;
@@ -180,13 +236,77 @@ export default {
 	margin-top: 3px; 
 }
 
-.bottom_region {
+.right_tab_area {
+	position: absolute;
+	background-color: cyan;
+	right: 0;
+	top: 0;
+	width: 26px;
+}
+
+.bottom_tab_area {
+	position: absolute;
+	background-color: green;
+	left: 0;
+	bottom: 0;
+	height: 26px;
+}
+/*
+.right_tab {
+	display: inline-block;
+	font-size: 17px;
+	writing-mode: vertical-lr;
+	background-color: black;
+	color: white;
+	padding: 2px;
+}
+*/
+.right_tab {
+	display: inline-block;
+	font-size: 17px;
+	writing-mode: horizontal-tb;
+	background-color: black;
+	color: white;
+	padding-left: 4px;
+	padding-bottom: 2px;
+	padding-top: 2px;
+}
+
+.bottom_tab {
+	display: inline-block;
+	font-size: 17px;
+	background-color: black;
+	writing-mode: horizontal-tb;
+	color: white;
+	padding: 2px;
+}
+
+.right_tab_selected {
+	display: inline-block;
+	font-size: 17px;
+	writing-mode: horizontal-tb;
+	background-color: gray;
+	color: white;
+	padding-left: 4px;
+	padding-bottom: 2px;
+	padding-top: 2px;
+}
+
+.bottom_tab_selected {
+	display: inline-block;
+	font-size: 17px;
+	background-color: gray;
+	writing-mode: horizontal-tb;
+	color: white;
+	padding: 2px;
+}
+.bottom {
 	position: absolute;
 	background-color: pink;
 	left: 0;
 	bottom: 0;
 	margin-left: 3px; 
-	margin-bottom: 3px; 
+	margin-bottom: 3px;
 }
 
 .outer_div {
@@ -202,7 +322,6 @@ export default {
 	left: 3px;
 	top: 3px;
 	padding: 3px;
-	border-style: solid;
-	border-color: #41719c;
+	border: 3px solid #41719c;
 }
 </style>
