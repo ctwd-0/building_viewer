38f8d08eee0c7b5c67fbfd9a6956bc2663a1e40a
ctwd-0 <suncong10@gmail.com>
2018-05-05 18:21:49 +0800

    在任意位置能够直接进入。

--- src/components/RightBar/Direct.vue
+++ src/components/RightBar/Direct.vue
@@ -2,7 +2,7 @@
 	<div class="direct" :style="{width:width - 20 + 'px'}">
 		<div class="direct_lable">编号入口：</div>
 		<div class="direct_input_line">
-			<input type="text" v-model="direct_id" :style="{width:width - 82 + 'px'}" >
+			<input type="text" v-model="table_id" :style="{width:width - 82 + 'px'}" >
 			<button class="direct_button" @click="direct_click">进入</button>
 		</div>
 	</div>
@@ -18,29 +18,12 @@ export default {
 	},
 	data () {
 		return {
-			direct_id: "",
+			table_id: "",
 		};
 	},
 	methods: {
 		direct_click() {
-			if(current_model !== 'g_-1') {
-				alert('请返回顶层');
-			} else if(this.direct_id === ''){
-				alert('请输入正确的编号');
-			} else {
-				var name = this.direct_id;
-				if(csv_rhino[name] !== undefined) {
-					name = csv_rhino[name];
-				}
-				var object_id = lookup_object_id(name);
-				if(object_id == -1) {
-					alert('请输入正确的编号');
-				} else if(object_id == -2){
-					alert('该编号对应重复的构件，请检查');
-				} else {
-					triger_direct_to_object(object_id);
-				}
-			}
+			triger_mv_direct_to_object(this.table_id)
 		},
 	},
 }
